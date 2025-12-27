214a60d22fda040d707908665c71dc60f43cccab
Ying Huang <huangyingw@gmail.com>
2025-12-26 17:21:47 -0800

    feat: 添加 spinup 库用于 Docker 容器训练
    spinup (OpenAI Spinning Up) 是 PPO/CPPO 训练的依赖
    由于 spinningup 与 Python 3.12 不兼容，直接复制源码

---
+++ spinup/__init__.py
@@ -0,0 +1,8 @@
+# Modified spinup - minimal imports for CPPO training
+# Only import utils (no algorithm imports to avoid gym dependency)
+
+# Loggers
+from spinup.utils.logx import Logger, EpochLogger
+
+# Version
+from spinup.version import __version__
